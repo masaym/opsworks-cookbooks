8754e676b82e765ec93d51a25c101d1f5a778e74
masaym <masa.y.m@gmail.com>
2014-11-16 22:31:58 +0900

    update redis config

---
+++ redis/recipes/config.rb
@@ -0,0 +1,25 @@
+node[:deploy].each do |app_name, deploy_config|
+  # determine root folder of new app deployment
+  app_root = "#{deploy_config[:deploy_to]}/current"
+
+  # use template 'redis.yml.erb' to generate 'config/redis.yml'
+  template "/etc/redis.conf" do
+    source "redis.conf.erb"
+    # cookbook "redis-config"
+
+    # set mode, group and owner of generated file
+    mode "0660"
+    group redis
+    owner redis
+
+    # define variable “@redis” to be used in the ERB template
+    variables(
+      :redis => deploy_config[:redis] || {}
+    )
+
+    # only generate a file if there is Redis configuration
+    not_if do
+      deploy_config[:redis].blank?
+    end
+  end
+end
