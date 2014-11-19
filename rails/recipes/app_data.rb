52b3294f925e0614a5df5439f995defb8f7d6bb7
masaym <masa.y.m@gmail.com>
2014-11-19 18:11:46 +0900

    update app_data recipe

--- rails/recipes/app_data.rb
+++ rails/recipes/app_data.rb
@@ -1,7 +1,8 @@
 node[:deploy].each do |app_name, deploy_config|
 
   file File.join(deploy_config[:deploy_to], 'shared', 'config', 'app_data.yml') do
-    content YAML.dump(deploy_config[:environment])
+    Chef.log.info("here!!:#{deploy_config[:environment]}")
+    content YAML.dump(deploy_config[:environment].to_hash)
   end
 
 end
