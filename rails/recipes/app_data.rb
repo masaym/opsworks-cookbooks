076285fb6639e954cae03b9abbf037dee5b7b345
masaym <masa.y.m@gmail.com>
2014-11-19 18:42:07 +0900

    update rails app_data recipe

--- rails/recipes/app_data.rb
+++ rails/recipes/app_data.rb
@@ -1,8 +1,14 @@
 node[:deploy].each do |app_name, deploy_config|
 
   file File.join(deploy_config[:deploy_to], 'shared', 'config', 'app_data.yml') do
-    Chef::Log.info("here!!:#{deploy_config[:environment]}")
     content YAML.dump(deploy_config[:environment].to_hash)
   end
+
+  execute "create symbolic link" do
+    command <<-EOF
+      cd #{File.join(deploy_config[:deploy_to], 'current', 'config')}
+      ln -s #{File.join(deploy_config[:deploy_to], 'shared', 'config', 'app_data.yml')} app_data.yml
+    EOF
+  end 
 
 end
